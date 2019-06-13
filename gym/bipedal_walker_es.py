05b9ce89025465eadd4debfa7d9b00f8b2313b83
ouj <jiawei.ou@gmail.com>
2019-06-12 23:06:50 -0700

    Update bipedal_walker_es.py

--- gym/bipedal_walker_es.py
+++ gym/bipedal_walker_es.py
@@ -10,40 +10,6 @@ def set_random_seed(seed):
     tf.set_random_seed(seed)
     np.random.seed(seed)
 
-class EvoModel:
-    def __init__(self, state_dim, action_dim):
-        self.state_dim = state_dim
-
-#        initializer = tf.keras.initializers.random_normal(mean=0.0, stddev=1.0)
-#        inputs = tf.keras.layers.Input(shape=(state_dim,))
-#        dense1 = tf.keras.layers.Dense(
-#            units=100,
-#            kernel_initializer=initializer,
-#            activation=tf.nn.relu)(inputs)
-#        outputs = tf.keras.layers.Dense(
-#            units=action_dim,
-#            kernel_initializer=initializer,
-#            activation=tf.nn.tanh)(dense1)
-#        self.model = tf.keras.Model(inputs=inputs, outputs=outputs)
-        hl_size = 100
-        self.model = []
-        self.model.append(np.random.randn(24, hl_size) / np.sqrt(24))
-        self.model.append(np.random.randn(hl_size, 4) / np.sqrt(hl_size))
-
-    def get_weights(self):
-        return self.model
-
-    def set_weights(self, weights):
-        self.model = weights
-
-    def predict(self, state):
-#        action = self.model.predict(np.atleast_2d(state))[0]
-        hl = np.matmul(state, self.model[0])
-        hl = np.tanh(hl)
-        action = np.matmul(hl, self.model[1])
-        action = np.tanh(action)
-        return action
-
 
 class Agent:
     def __init__(
@@ -58,11 +24,26 @@ class Agent:
         self.sigma = sigma
         self.population_size = population_size
 
-    def play_episode(self, model, max_iterations=300, render=False):
+    def create_network_model(self, state_dim, action_dim, layer_sizes=(100, )):
+        model = []
+        dim = state_dim
+        for h in layer_sizes[:-1]:
+            model.append(np.random.randn(dim, h) / np.sqrt(dim))
+            dim = h
+        model.append(np.random.randn(dim, action_dim) / np.sqrt(action_dim))
+        return model
+
+    def predict(self, model, state):
+        x = state
+        for w in model:
+            x = np.tanh(np.matmul(x, w))
+        return x
+
+    def play_episode(self, model, episode_iterations=300, render=False):
         observation = self.env.reset()
         done = False
         episode_return = 0
-        for _ in range(max_iterations):
+        for _ in range(episode_iterations):
             action = self.get_action(model, observation)
             observation, reward, done, _ = self.env.step(action)
             episode_return += reward
@@ -73,38 +54,38 @@ class Agent:
         return episode_return
 
     def get_action(self, model, state):
-        return model.predict(state)
+        return self.predict(model, state)
 
-    def explore(self, model):
-        weights = model.get_weights()
-
+    def explore(self, model, episode_iterations=500):
         N = [] # mutations
         R = np.zeros(self.population_size) # returns
-        for v in weights:
+        for v in model:
             N.append(np.random.randn(self.population_size, v.shape[0], v.shape[1]))
 
         for p in range(self.population_size):
-            weights_try = []
-            for v, n in zip(weights, N):
-                weights_try.append(v + self.sigma * n[p])
+            new_model = []
+            for v, n in zip(model, N):
+                new_model.append(v + self.sigma * n[p])
 
-            model.set_weights(weights_try)
-            R[p] = self.play_episode(model, render=False)
+            R[p] = self.play_episode(
+                new_model, episode_iterations, render=False
+            )
 
         A = (R - np.mean(R)) / np.std(R)
-        for k in range(len(weights)):
-            weights[k] = weights[k] + self.learning_rate/(self.population_size * self.sigma) * np.dot(N[k].transpose(1, 2, 0), A)
-        model.set_weights(weights)
+        for k in range(len(model)):
+            model[k] = model[k] + self.learning_rate / (self.population_size * self.sigma) * np.dot(N[k].transpose(1, 2, 0), A)
+
+        return model
 
     def train(self, iterations=100):
-        model = EvoModel(
+        model = self.create_network_model(
             self.env.observation_space.shape[0],
             self.env.action_space.shape[0]
         )
         avg_reward = None
         for t in range(iterations):
             t0 = datetime.now()
-            self.explore(model)
+            model = self.explore(model)
             reward = self.play_episode(model, render=(t % 10 == 0))
             if avg_reward is None:
                 avg_reward = reward
@@ -119,9 +100,8 @@ class Agent:
             )
 
 
-
-    def save_weights(self, weights, filename="evolution.npy"):
-        np.savetxt(filename, weights)
+    def save_weights(self, model, filename="evolution.npy"):
+        np.savetxt(filename, model)
 
     def load_weights(self, filename="evolution.npy"):
         if not os.path.exists(filename):
