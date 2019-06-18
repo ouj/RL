2951f247fe9fcf01d3a265b5736a1dfaf2dea85b
Jiawei Ou <ouj@fb.com>
2019-06-18 10:34:05 -0700

    Fix the bug when using reduce_max without providing the proper axis.

--- gym/cart_pole_dqn.py
+++ gym/cart_pole_dqn.py
@@ -4,18 +4,15 @@ import gym
 import numpy as np
 import tensorflow as tf
 from rl.replay_buffer import ReplayBuffer
-import matplotlib as mpl
-mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 #%% Set random seeds
 def set_random_seed(seed):
     tf.set_random_seed(seed)
-    np.random.seed(seed)  
+    np.random.seed(seed)
 set_random_seed(0)
 
 #%% Helper functions
-
 def get_vars(scope):
     return [x for x in tf.global_variables() if scope in x.name]
 
@@ -37,25 +34,29 @@ tf.reset_default_graph()
 
 with tf.variable_scope("main"):
     x = tf.placeholder(
-        shape=(None, env.observation_space.shape[0]), 
-        dtype=tf.float32, 
+        shape=(None, env.observation_space.shape[0]),
+        dtype=tf.float32,
         name="x"
     )
-    w = tf.keras.layers.Dense(units=10, activation=tf.nn.relu, name="w")(x)
-    q = tf.keras.layers.Dense(units=env.action_space.n, name="q")(w)
+    w = tf.keras.layers.Dense(units=20, activation=tf.nn.relu, name="w")(x)
+    v = tf.keras.layers.Dense(units=20, activation=tf.nn.relu, name="v")(w)
+    q = tf.keras.layers.Dense(units=env.action_space.n, name="q")(v)
 
 with tf.variable_scope("target"):
     x2 = tf.placeholder(
-        shape=(None, env.observation_space.shape[0]), 
+        shape=(None, env.observation_space.shape[0]),
         dtype=tf.float32,
         name="x2"
     )
     w2 = tf.keras.layers.Dense(
-        units=10, activation=tf.nn.relu, name="w", trainable=False
+        units=20, activation=tf.nn.relu, name="w", trainable=False
     )(x2)
+    v2 = tf.keras.layers.Dense(
+        units=20, activation=tf.nn.relu, name="v", trainable=False
+    )(w2)
     q_target = tf.keras.layers.Dense(
         units=env.action_space.n, name="q", trainable=False
-    )(w2)
+    )(v2)
 
 
 r = tf.placeholder(dtype=tf.float32, shape=(None,)) # reward
@@ -67,14 +68,21 @@ selected_q = tf.reduce_sum(
     reduction_indices=[1]
 )
 
+next_q = tf.math.reduce_max(q_target, axis=1)
 g = tf.stop_gradient(
-    r + GAMMA * (1 - d) * tf.math.reduce_max(q_target)
+    r + GAMMA * next_q * (1 - d)
 )
 q_loss = tf.reduce_sum(tf.square(selected_q - g))
 
 train_op = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(
     q_loss
 )
+
+print([
+   (v_main.name, v_targ.name) for v_main, v_targ in zip(
+         get_vars('main'), get_vars('target')
+   )
+])
 
 # Copy main network params to target networks
 target_init = tf.group([
@@ -137,9 +145,10 @@ def train():
             r: batch['r'],
             d: batch['d']
         }
+        session.run(train_op, feed_dict)
         session.run(target_update)
         return session.run(q_loss, feed_dict)
-        
+
 #%% Overfit
 def overfitting():
     for _ in range(steps):
@@ -157,27 +166,32 @@ def overfitting():
         session.run(target_update)
 
 # %% main loop
-N = 500
+N = 20000
 losses = []
 returns = []
 for n in range(N):
     epsilon = 1.0 / np.sqrt(n+1)
     steps, total_return = play_once(epsilon)
-    
+
     returns.append(total_return)
     if MINIMAL_SAMPLES < replay_buffer.number_of_samples():
         loss = train()
         losses.append(losses)
-    else:
-        print ("Not enough samples", replay_buffer.number_of_samples())
-        
+
     if n != 0 and n % 10 == 0:
         print(
-            "Episode:", n, 
-            "Returns:", total_return, 
+            "Episode:", n,
+            "Returns:", total_return,
             "epsilon:", epsilon
         )
         
+#%% Demo
+N = 10
+for n in range(N):
+    play_once(0.0, render=True)
+        
+#%% Report
+
 plt.plot(losses)
 plt.title("Q Losses")
 plt.show()
