cdd9a19809c905b8146685d09d297d2b512009a0
Jiawei Ou <ouj@fb.com>
2019-06-28 00:16:42 -0700

    Rewrite DDPG

--- gym/bipedal_walker_ddpg.py
+++ gym/bipedal_walker_ddpg.py
@@ -1,24 +1,112 @@
 #!/usr/bin/env python3
 
 import os
+
 import gym
 import numpy as np
 import tensorflow as tf
-from rl.replay_buffer import ReplayBuffer
+from rl.helpers import set_random_seed
+from rl.mlp import MLPNetwork
+
+
+# Environment
+env = gym.make("BipedalWalker-v2")
 
 # Configurations
 LEARNING_RATE = 1e-3
 
-# Set random seeds
-def set_random_seed(seed):
-    tf.set_random_seed(seed)
-    np.random.seed(seed)
 set_random_seed(0)
 
 
-# Setup
-env = gym.make("BipedalWalker-v2")
+class MuNetwork(MLPNetwork):
+    def __init__(self, output_dim, activation=tf.nn.relu, trainable=True):
+        super(MLPNetwork, self).__init__()
+        self.layers = [
+            tf.layers.Dense(
+                units=512,
+                activation=activation,
+                trainable=trainable,
+                name="W",
+                kernel_initializer=tf.initializers.glorot_normal,
+            ),
+            tf.layers.Dense(
+                units=512,
+                activation=activation,
+                trainable=trainable,
+                name="V",
+                kernel_initializer=tf.initializers.glorot_normal,
+            ),
+            tf.layers.Dense(
+                units=output_dim,
+                trainable=trainable,
+                name="MU",
+                kernel_initializer=tf.initializers.glorot_normal,
+            ),
+        ]
+
+
+class QNetwork(tf.layers.Layer):
+    def __init__(self, activation=tf.nn.relu, trainable=True):
+        super(MuNetwork, self).__init__()
+        self.layers = [
+            tf.layers.Dense(
+                units=512,
+                activation=activation,
+                trainable=trainable,
+                name="W",
+                kernel_initializer=tf.initializers.glorot_normal,
+            ),
+            tf.layers.Dense(
+                units=512,
+                activation=activation,
+                trainable=trainable,
+                name="V",
+                kernel_initializer=tf.initializers.glorot_normal,
+            ),
+            tf.layers.Dense(
+                units=1,
+                trainable=trainable,
+                name="Q",
+                kernel_initializer=tf.initializers.glorot_normal,
+            ),
+        ]
+
+
+class ReplayBuffer:
+    def __init__(self, observation_dim, action_dim, max_size=1000000):
+        self.index = 0
+        self.size = 0
+        self.max_size = max_size
+        self.states = np.zeros([max_size], dtype=np.float32)
+        self.next_states = np.zeros([max_size, observation_dim], dtype=np.float32)
+        self.actions = np.zeros([max_size, action_dim], dtype=np.float32)
+        self.rewards = np.zeros(max_size, dtype=np.float32)
+        self.dones = np.zeros(max_size, dtype=np.float32)
+
+    def number_of_samples(self):
+        return self.size
+
+    def store(self, observation, action, reward, next_observation, done):
+        self.states[self.index] = observation
+        self.next_states[self.index] = next_observation
+        self.actions[self.index] = action
+        self.rewards[self.index] = reward
+        self.dones[self.index] = done
+        self.index = (self.index + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
+
+    def sample_batch(self, batch_size=32):
+        assert batch_size <= self.size
+        indexes = np.random.randint(0, self.size, size=batch_size)
+        return dict(
+            s=self.states[indexes],
+            s2=self.next_states[indexes],
+            a=self.actions[indexes],
+            r=self.rewards[indexes],
+            d=self.dones[indexes],
+        )
+
+
 replay_buffer = ReplayBuffer(
-    observation_shape=env.observation_space.shape,
-    action_shape=env.action_space.shape
+    observation_dim=env.observation_space.shape[0], action_dim=env.action_space.shape[0]
 )
---
+++ gym/rl/mlp.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+
+class MLPNetwork(tf.layers.Layer):
+    def __init__(self):
+        super(MuNetwork, self).__init__()
+        self.layers = []
+
+    def collect_variables(self):
+        variables = []
+        for layer in self.layers:
+            variables += layer.variables
+        return variables
+
+    def copy_from(self, other_qlayer):
+        assert isinstance(other_qlayer, self.__class__)
+        target_variables = self.collect_variables()
+        source_variables = other_qlayer.collect_variables()
+        copy_op = tf.group(
+            [
+                tf.assign(v_tgt, v_src)
+                for v_tgt, v_src in zip(target_variables, source_variables)
+            ]
+        )
+        return copy_op
+
+    def update_from(self, other_qlayer, decay):
+        assert isinstance(other_qlayer, self.__class__)
+        target_variables = self.collect_variables()
+        source_variables = other_qlayer.collect_variables()
+        update_op = tf.group(
+            [
+                tf.assign(v_tgt, decay * v_tgt + (1 - decay) * v_src)
+                for v_tgt, v_src in zip(target_variables, source_variables)
+            ]
+        )
+        return update_op
+
+    def is_equal(self, other_qlayer, session=None):
+        assert isinstance(other_qlayer, self.__class__)
+        target_variables = self.collect_variables()
+        source_variables = other_qlayer.collect_variables()
+        equal_op = tf.reduce_all(
+            [
+                tf.reduce_all(tf.equal(v_tgt, v_src))
+                for v_tgt, v_src in zip(target_variables, source_variables)
+            ]
+        )
+        return equal_op
+
+    def setup_tensorboard(self):
+        variables = self.collect_variables()
+        for v in variables:
+            tf.summary.histogram(v.name, v)
+
+    def call(self, x):
+        for l in self.layers:
+            x = l(x)
+        return x
